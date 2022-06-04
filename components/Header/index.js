089ef0a764d81f906b0dfa1b224e7f1dfd3ea233
Princenick-BL <princenick@gmail.com>
2022-06-04 12:48:40 +0200

    updates

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,16 +8,27 @@ export default function Header() {
 
 export function ArticleHeader(){
     return(
-        <header className={styles.articleHeader}>
-            <nav>
-                <div className={styles.logo}>logo</div>
-                <ul>
-                    {/* <li>menu1</li>
-                    <li>menu2</li>
-                    <li>menu3</li> */}
-                </ul>
-                <div className={styles.logo}>logo</div>
-            </nav>   
+        <header
+            className="ampstart-headerbar fixed flex justify-start items-center top-0 left-0 right-0 pl2 pr4"
+        >
+            <div
+                role="button"
+                aria-label="open sidebar"
+                on="tap:header-sidebar.toggle"
+                tabIndex="0"
+                className="ampstart-navbar-trigger pr2"
+            >
+                ☰
+            </div>
+            <amp-img
+                src="../img/blog/logo.png"
+                width="100"
+                height="61.3"
+                layout="fixed"
+                className="my0 mx-auto"
+                alt="The Blog"
+            ></amp-img>
         </header>
+
     )
 }
