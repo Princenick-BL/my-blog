542440bc6d5d4587d2496f60610a5d133415355b
Princenick-BL <princenick@gmail.com>
2022-06-22 00:00:26 +0200

    push

--- components/StoryPreview/index.js
+++ components/StoryPreview/index.js
@@ -3,7 +3,7 @@ import styles from './index.module.scss'
 
 export default function StoryPreview({url,img,title,logo}) {
   return (
-      <a target={"_blank"} href={url}>
+      <a target={"_blank"} rel="noreferrer" href={url}>
         <div className={styles.storyPreview} style={{backgroundImage:`url(${img})`}}>
             <div className={styles.title}>{title}</div>
             <div className={styles.logo} style={{backgroundImage:`url(${logo})`}}></div>
