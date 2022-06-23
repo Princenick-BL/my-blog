166f0226b8c9f36af4cb9a391222bea30f3b93b6
Princenick-BL <princenick@gmail.com>
2022-06-24 00:03:29 +0200

    updates

--- components/Caroussel/index.js
+++ components/Caroussel/index.js
@@ -2,20 +2,19 @@ import React from 'react'
 import styles from './index.module.scss'
 export default function Caroussel() {
     return (
-        <div className={styles.carouselWrapper}>
-            <div className={styles.carouselContainer}>
-                <div className={styles.carousel}>
-                    <div className={styles.imageOne}>
-                        Article 1
-                    </div>
-                    <div className={styles.imageTwo}>
-                        Article 2
-                    </div>
-                    <div className={styles.imageThree}>
-                        Article 3
-                    </div>
+        <div className={styles.carouselContainer}>
+            <div className={styles.carousel}>
+                <div className={styles.image + " " + styles.imageOne}>
+                    Article 1
+                </div>
+                <div className={styles.image + " " + styles.imageTwo}>
+                    Article 2
+                </div>
+                <div className={styles.image + " " + styles.imageThree}>
+                    Article 3
                 </div>
             </div>
         </div>
+
     )
 }
