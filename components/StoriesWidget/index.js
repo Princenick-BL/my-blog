6b62a4f5aceb9410de29952d5a0d880d52f71295
Princenick-BL <princenick@gmail.com>
2022-07-09 23:07:38 +0200

    updates

--- components/StoriesWidget/index.js
+++ components/StoriesWidget/index.js
@@ -6,64 +6,87 @@ import { getArticle } from '../../services/articles'
 import Image from 'next/image'
 import Link from 'next/link'
 import { categories } from '../../constants'
-import { Carousel } from 'react-responsive-carousel';
+
+import Carousel from 'react-material-ui-carousel'
 
 
+export default function Example(props){
 
-export default function Caroussel() {
   return (
-    <div className={styles.container} style={{height:"max-content"}}>
-      <Carousel
-      
-      >
 
+      <Carousel className={styles.container} > 
         {categories.map((res,index)=>{
-          return (
-            <div style={{height:"max-content",paddingBottom:"50px"}}  key={index}>
-              <Image
-                src={`/img/${res?.img}`}
-                width={700}
-                height={700 * 9 / 16}
-              />
-              <div className={styles.info}>
-                <h1 className={styles.cat}>
-                  {res?.name?.toUpperCase()}
-                </h1>
-                <p>
-                  {res?.description}
-                </p>
-                <div className={styles.link}>
-                  Découvrir
-                </div>
-              </div>
-            </div>
-            
-          )
-        })}
-      </Carousel>
-      {/* <div>
-        <Image
-          src={`/img/${categories[0]?.img}`}
-          width={700}
-          height={700 * 9 / 16}
-        />
-        <div className={styles.info}>
-          <h1 className={styles.cat}>
-            {categories[0]?.name?.toUpperCase()}
-          </h1>
-          <p>
-            {categories[0]?.description}
-          </p>
-          <Link href={categories[0]?.name?.toLocaleLowerCase()}>
-            <a>
+        return (
+          <div style={{height:"max-content",paddingBottom:"50px"}}  key={index}>
+            <Image
+              src={`/img/${res?.img}`}
+              width={700}
+              height={700 * 9 / 16}
+            />
+            <div className={styles.info}>
+              <h1 className={styles.cat}>
+                {res?.name?.toUpperCase()}
+              </h1>
+              <p>
+                {res?.description}
+              </p>
               <div className={styles.link}>
                 Découvrir
               </div>
-            </a>
-          </Link>
-        </div>
-      </div>       */}
-    </div>
+            </div>
+          </div>
+          
+        )
+        })}
+      </Carousel>
   )
 }
+
+function Item(props)
+{
+    return (
+        <Paper>
+            <h2>{props.item.name}</h2>
+            <p>{props.item.description}</p>
+
+            <Button className="CheckButton">
+                Check it out!
+            </Button>
+        </Paper>
+    )
+}
+
+// export default function Caroussel() {
+//   return (
+//     <div className={styles.container} style={{height:"max-content"}}>
+    
+
+//         {/* {categories.map((res,index)=>{
+//           return (
+//             <div style={{height:"max-content",paddingBottom:"50px"}}  key={index}>
+//               <Image
+//                 src={`/img/${res?.img}`}
+//                 width={700}
+//                 height={700 * 9 / 16}
+//               />
+//               <div className={styles.info}>
+//                 <h1 className={styles.cat}>
+//                   {res?.name?.toUpperCase()}
+//                 </h1>
+//                 <p>
+//                   {res?.description}
+//                 </p>
+//                 <div className={styles.link}>
+//                   Découvrir
+//                 </div>
+//               </div>
+//             </div>
+            
+//           )
+//         })} */}
+        
+     
+//     </div>
+//   )
+// }
 
