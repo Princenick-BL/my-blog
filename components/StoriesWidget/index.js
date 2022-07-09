c866d6c52280480b4970ac827034c1ca9ae30f72
Princenick-BL <princenick@gmail.com>
2022-07-09 19:12:18 +0200

    updates

--- components/StoriesWidget/index.js
+++ components/StoriesWidget/index.js
@@ -6,13 +6,42 @@ import { getArticle } from '../../services/articles'
 import Image from 'next/image'
 import Link from 'next/link'
 import { categories } from '../../constants'
+import { Carousel } from 'react-responsive-carousel';
 
 
 
 export default function Caroussel() {
   return (
     <div className={styles.container}>
-      <div>
+      <Carousel
+       
+      >
+
+        {categories.map((res,index)=>{
+          return (
+            <div style={{minHeight:"500px"}} key={index}>
+              <Image
+                src={`/img/${res?.img}`}
+                width={700}
+                height={700 * 9 / 16}
+              />
+              <div className={styles.info}>
+                <h1 className={styles.cat}>
+                  {res?.name?.toUpperCase()}
+                </h1>
+                <p>
+                  {res?.description}
+                </p>
+                <div className={styles.link}>
+                  Découvrir
+                </div>
+              </div>
+            </div>
+            
+          )
+        })}
+      </Carousel>
+      {/* <div>
         <Image
           src={`/img/${categories[0]?.img}`}
           width={700}
@@ -25,75 +54,16 @@ export default function Caroussel() {
           <p>
             {categories[0]?.description}
           </p>
-          <div className={styles.link}>
-            Découvrir
-          </div>
-        </div>
-      </div>
-      {/* {categories.map((res,index)=>{
-        return(
-          <Link key={index} href={`/${res?.name?.toLowerCase()}`}>
+          <Link href={categories[0]?.name?.toLocaleLowerCase()}>
             <a>
-              <div className={styles.card} 
-                style={{
-                  backgroundImage:`url(/img/${res?.img})`
-                }}
-              >
-
-                <div className={styles.cat}>
-                  {res?.name?.toUpperCase()}
-                </div>
-
-                <div className={styles.calc}>
-                  <div className={styles.button}>Découvrir</div>
-                </div>
-
+              <div className={styles.link}>
+                Découvrir
               </div>
             </a>
           </Link>
-        )
-      })} */}
-      
+        </div>
+      </div>       */}
     </div>
   )
 }
 
-
-// import React,{useState,useEffect} from 'react'
-// import styles from './index.module.scss'
-// import Loading from '../../Loading'
-// import { getTopStories } from '../../services/stories'
-// import { getArticle } from '../../services/articles'
-
-// export default function StoriesWidget() {
-
-//     const [stories,setStories] =  useState([])
-//     useEffect(()=>{
-//       (async ()=>{
-//         const res = await getTopStories()
-//         setStories(res.data)
-//       })();
-//     },[])
-
-//     return (
-//         <div classNameName={styles.list}>
-//             {stories ? stories?.map((story,index)=>{
-//               return(
-//                   <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
-//                     <div  classNameName={styles.outerCorner}>
-//                         <div 
-//                             classNameName={styles.innerConner}
-//                             style={{backgroundImage:`url(${story?.poster})`}}
-//                         ></div>
-//                     </div>
-
-//                   </a>
-                
-//               )
-//             }):(
-//               <Loading/>
-//             )}
-            
-//         </div>
-//     )
-// }
