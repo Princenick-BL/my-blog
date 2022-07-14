44473b1b2dbbfaa2100732b8a02c69933503099b
Princenick-BL <princenick@gmail.com>
2022-07-15 01:52:01 +0200

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.scss'
 import '../styles/quill.css'
+import WaitingPage from '../components/WaitingPage'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WaitingPage>
+      <Component {...pageProps} />
+    </WaitingPage>
+  )
 }
 
 export default MyApp
